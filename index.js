2f5a6a98fb87b3db2fca204d858f809c8a3f8d68
edzillion <edzillion@gmail.com>
2018-06-10 14:46:57 +0200

    env

--- index.js
+++ index.js
@@ -44,8 +44,7 @@ class RocketChatBotAdapter extends Adapter {
     settings.username = process.env.ROCKETCHAT_USER || 'bot'
     settings.password = process.env.ROCKETCHAT_PASS || 'bot'
     settings.useSsl = (!process.env.ROCKETCHAT_USESSL || process.env.ROCKETCHAT_USESSL === '') ? true : process.env.ROCKETCHAT_USESSL  // server uses https ?
-    settings.allPublic = true
-    //settings.rooms = (process.env.ROCKETCHAT_ROOM === '') ? ['GENERAL'] : [process.env.ROCKETCHAT_ROOM];
+    settings.rooms = (!process.env.ROCKETCHAT_ROOM || process.env.ROCKETCHAT_ROOM === '') ? ['GENERAL'] : [process.env.ROCKETCHAT_ROOM];
     this.settings = settings
 
     // Print logs with current configs
